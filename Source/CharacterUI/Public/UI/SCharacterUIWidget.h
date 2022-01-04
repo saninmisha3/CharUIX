3d0c1e3fd36c75e1bfd2378308d344b3cb39ae54
saninmisha3 <saninmisha3@gmail.com>
2022-01-04 07:11:28 +0100

    Added the SCharacterUIWidget class

---
+++ Source/CharacterUI/Public/UI/SCharacterUIWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SCharacterUIWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARACTERUI_API USCharacterUIWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
